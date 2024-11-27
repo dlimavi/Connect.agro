2524703b7f4764e8a03374908209c471f4063b86
MiguelD3V <142622308+MiguelD3V@users.noreply.github.com>
2024-11-26 23:11:08 -0300

    Add files via upload

--- obj/Release/net8.0/Connect.Agro.AssemblyInfo.cs
+++ obj/Release/net8.0/Connect.Agro.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connect.Agro")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2622710cb82b67b17b721fd76abe9967c79af5d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connect.Agro")]
